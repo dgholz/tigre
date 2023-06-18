a0d32d7c14ed20c26845d8865ad8cb274dca3da3
Daniel George Holz <dgholz@gmail.com>
2023-06-18 23:13:44 +0100

    Get (some of) user's repos

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ type Tigre struct {
 	user   string
 }
 
+type Repo struct {
+	Name string
+}
+
 func main() {
 	client, err := api.DefaultRESTClient()
 	if err != nil {
@@ -21,6 +25,11 @@ func main() {
 
 	tigre.set_user()
 	fmt.Printf("%+v\n", tigre)
+	repos := make(chan Repo)
+	go tigre.user_repos(repos)
+	for repo := range repos {
+		fmt.Printf("%+v\n", repo)
+	}
 }
 
 func (t *Tigre) set_user() {
@@ -33,4 +42,17 @@ func (t *Tigre) set_user() {
 		log.Fatal(err)
 	}
 	t.user = response.Login
+}
+
+func (t Tigre) user_repos(c chan Repo) {
+	var response []Repo
+	err := t.client.Get(fmt.Sprintf("users/%s/repos", t.user), &response)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, repo := range response {
+		c <- repo
+	}
+	close(c)
 }
